2ab6f18f4fb894ea100cdefe26df60b9181c6ff8
Mayank Shukla <117716383+mayankshukla-20@users.noreply.github.com>
2023-12-18 13:40:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstNonRepeatedChar(str) {
  // Write your code here
+	let i=0;
+	while(i<str.length){
+		if(str[i] === str[i+1]){
+			i += 2;
+		}
+		else{ 
+			return str[i];
+		}
+	}
+	return null;
 }
 const input = prompt("Enter a string");
 alert(firstNonRepeatedChar(input)); 
